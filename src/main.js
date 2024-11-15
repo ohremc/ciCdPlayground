2c0b17aca63fec3ba90cf2e8b61ec9316ef6eb9c
ohremc <christoph.ohrem@tngtech.com>
2024-11-15 14:07:24 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Christoph Ohrem',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'GitLab'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'GitLab',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'Learn some basics'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'read', 
+			'gym', 
+			'computer games'
 		]
 	}
 });
